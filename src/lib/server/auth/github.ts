21c6c543e877fdd242da993495f7ce5a37c0b869
Chad Freeman <ceo@marinadev.xyz>
2024-11-02 10:44:21 -0400

    change env variables for github actions

--- src/lib/server/auth/github.ts
+++ src/lib/server/auth/github.ts
@@ -1,5 +1,5 @@
-import { GITHUB_ID, GITHUB_SECRET } from '$env/static/private';
+import { ID_GITHUB, SECRET_GITHUB } from '$env/static/private';
 
 import { GitHub } from 'arctic';
 
-export default new GitHub(GITHUB_ID, GITHUB_SECRET);
+export default new GitHub(ID_GITHUB, SECRET_GITHUB);
