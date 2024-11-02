2b6586141615e98f0f83d53ce5fe2ed3debbf974
Chad Freeman <ceo@marinadev.xyz>
2024-11-02 11:00:31 -0400

    this is gonna be really bad i hope i can delete the logs

--- src/lib/server/auth/github.ts
+++ src/lib/server/auth/github.ts
@@ -1,5 +1,5 @@
 import { ID_GITHUB, SECRET_GITHUB } from '$env/static/private';
-
+console.log(await import('$env/static/private'))
 import { GitHub } from 'arctic';
 
 export default new GitHub(ID_GITHUB, SECRET_GITHUB);
