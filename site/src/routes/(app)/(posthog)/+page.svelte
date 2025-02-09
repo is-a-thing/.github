69856754e032f31de801ca71e46bf2e8393138ac
Chad Freeman <ceo@marinadev.xyz>
2025-02-09 10:20:22 -0500

    keep lowercase vibe

--- site/src/routes/(app)/(posthog)/+page.svelte
+++ site/src/routes/(app)/(posthog)/+page.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div class="flex flex-col h-full items-center justify-center md:items-start md:pl-32 pb-16">
-	<h1 class="text-6xl">Free is-a-th.ing subdomain</h1>
+	<h1 class="text-6xl">free is-a-th.ing subdomain</h1>
 	<div class="flex flex-row gap-x-2">
 		<DomainInput {name} />
 	</div>
--- site/src/routes/(app)/(posthog)/dashboard/get/[domain]/+page.svelte
+++ site/src/routes/(app)/(posthog)/dashboard/get/[domain]/+page.svelte
@@ -15,10 +15,6 @@
 </script>
 
 <div class="flex flex-col h-full items-center justify-center md:items-start md:pl-32 pb-16">
-	<!-- <h1 class="text-6xl">Free is-a-th.ing subdomain</h1>
-	<div class="flex flex-row gap-x-2">
-		<DomainInput {name} />
-	</div> -->
 	{#if data.available}
 		{#if Object.entries(data.auth.domains).length >= data.auth.slots}
 			<span class="text-warning text-2xl"
