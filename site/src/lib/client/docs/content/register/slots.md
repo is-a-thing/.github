d4cd31f3be4f1c1363ae0739227e1e29cfda15d0
kai <kai@kaidoesthings.xyz>
2025-06-01 18:52:49 -0400

    Clear in order to prime for v2

--- site/src/lib/client/docs/content/register/slots.md
+++
@@ -1,3 +0,0 @@
-# what are slots?
-
-slots are simply the amount of domains you are allowed to hold at a time.
--- site/src/routes/(other)/docs/[...page]/+page.ts
+++
@@ -1,7 +0,0 @@
-export function load({ params: { page } }) {
-	return { page }
-}
-
-export const prerender = true
-export const trailingSlash = 'never'
-export const ssr = false
